24de337320964525478412ea614ff0f2fac1d1ae
Reginald Ferland <reginald@ferland.io>
2025-10-17 12:50:22 -0400

    Add wikitext_resume.sh script for resuming training on Wikitext dataset

---
+++ examples/wikitext_resume.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+python train.py \
+  --dataset-name wikitext \
+  --dataset-config wikitext-103-raw-v1 \
+  --text-column text \
+  --tokenizer-path tokenizers/bpe-32k \
+  --batch-size 8 \
+  --block-size 512 \
+  --max-iters 30000 \
+  --resume
